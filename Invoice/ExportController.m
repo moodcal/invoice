1fa1ab3f8a079645d8e5333250abd059ac9512b2
zhengpeng <zpha_qp@163.com>
2017-04-01 11:32:43 +0800

    change

--- Invoice/ExportController.m
+++ Invoice/ExportController.m
@@ -12,6 +12,8 @@
 @property (weak, nonatomic) IBOutlet UIButton *startButton;
 @property (weak, nonatomic) IBOutlet UIButton *endButton;
 @property (weak, nonatomic) IBOutlet UIButton *sendButton;
+@property (nonatomic,strong) UIView *blackLoadingView;
+
 - (IBAction)startAction:(id)sender;
 - (IBAction)endAction:(id)sender;
 - (IBAction)sendAction:(id)sender;
@@ -22,6 +24,19 @@
 @end
 
 @implementation ExportController
+
+- (UIView *)blackLoadingView
+{
+    if (!_blackLoadingView) {
+        _blackLoadingView = [[UIView alloc] initWithFrame:CGRectMake(0, 64, self.view.width, self.view.height-64)];
+        _blackLoadingView.backgroundColor = [UIColor clearColor];
+        
+        BlackLoadingView *loadingView = [[BlackLoadingView alloc] init];
+        loadingView.center = CGPointMake(_blackLoadingView.width/2, _blackLoadingView.height/2);
+        [_blackLoadingView addSubview:loadingView];
+    }
+    return _blackLoadingView;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -68,6 +83,10 @@
         return;
     }
     
+    if (!self.blackLoadingView.superview) {
+        [self.view addSubview:self.blackLoadingView];
+    }
+    
     AFHTTPSessionManager *sessionManager = [[SRApiManager sharedInstance] sessionManager];
     NSMutableDictionary *params = [NSMutableDictionary dictionary];
     [params setObject:self.startButton.titleLabel.text forKey:@"beginDate"];
@@ -76,6 +95,9 @@
     [sessionManager.requestSerializer setValue:params.signature forHTTPHeaderField:@"sign"];
     [sessionManager POST:ApiMethodInvoicsExport parameters:params progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
         DLog(@"response: %@", responseObject);
+        
+        [self.blackLoadingView removeFromSuperview];
+
         if ([[responseObject objectForKey:@"success"] boolValue]) {
         } else {
             if ([[responseObject objectForKey:@"error_code"] integerValue] == 401) {
@@ -86,6 +108,8 @@
         }
     } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
         DLog(@"error: %@", error);
+        [self.blackLoadingView removeFromSuperview];
+
     }];
 }
 
